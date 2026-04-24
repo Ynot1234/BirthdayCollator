4c0493d0109800459b75564cb8819d691058b577
Tony Vardin <anthonyvardin@yahoo.com>
2026-04-23 20:08:16 -0500

    Modified pipeline and added options config

---
+++ BirthdayCollator.Server/Configuration/BirthSourceOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace BirthdayCollator.Server.Configuration;
+
+public sealed class BirthSourceOptions
+{
+    public bool EnableYearParser { get; set; } = false;
+    public bool EnableDateParser { get; set; } = false;
+    public bool EnableCategoryParser { get; set; } = false;
+    public bool EnableGenarianParser { get; set; } = false;
+    public bool EnableOnThisDayParser { get; set; } = true;
+    public bool EnableImdbParser { get; set; } = false;
+}
